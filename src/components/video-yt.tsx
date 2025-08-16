b400c65237fc06df59d05eb541eecc67f67604c7
Manav Misra <manavm@visionwebsoft.com>
2025-08-16 12:22:49 -0500

    Add  🤬 videos

--- src/components/video-yt.tsx
+++ src/components/video-yt.tsx
@@ -1,5 +1,13 @@
-export function VideoYT({ url, title }: { url: string; title?: string }) {
-  let embedUrl: string;
+export function VideoYT({
+  url,
+  title,
+  caption,
+}: {
+  url: string;
+  title?: string;
+  caption?: string;
+}) {
+  let embedUrl = url;
 
   if (url.includes("youtu.be/")) {
     const videoId = url.split("youtu.be/")[1]?.split("?")[0];
@@ -7,18 +15,32 @@ export function VideoYT({ url, title }: { url: string; title?: string }) {
   } else if (url.includes("youtube.com/watch")) {
     const videoId = url.split("v=")[1]?.split("&")[0];
     embedUrl = `https://www.youtube.com/embed/${videoId}`;
-  } else embedUrl = url;
+  }
+
+  const YTParams = new URLSearchParams({
+    rel: "0", // Don't show related videos from other channels whenever paused
+  });
 
   return (
-    <div className="my-6 aspect-video w-full overflow-hidden rounded-xl">
-      <iframe
-        src={embedUrl}
-        title={title || "YouTube video"}
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-        width="100%"
-        height="100%"
-      />
-    </div>
+    <figure className="my-6">
+      {/*
+       * * Keep these classes in outer `div` to
+       * allow the `figcaption` to be visible.
+       */}
+      <div className="aspect-video w-full overflow-hidden rounded-xl">
+        <iframe
+          src={`${embedUrl}?${YTParams.toString()}`}
+          title={title || "YouTube video"}
+          allowFullScreen
+          width="100%"
+          height="100%"
+        />
+      </div>
+      {caption && (
+        <figcaption className="mt-3 text-sm text-gray-600 dark:text-gray-500">
+          {caption}
+        </figcaption>
+      )}
+    </figure>
   );
 }
